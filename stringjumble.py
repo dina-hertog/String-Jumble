f413b80fb6f0a0e5dc37115920c911faa020eb1a
dina-hertog <dinahertog-raz@hanovernorwichschools.org>
2015-10-15 14:41:07 -0400

    Updated from Brython Server: 10/15/2015 2:41:07 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,8 +25,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-text = input("Please enter a string of text (the bigger the better):")
-print('You entered "' + text + '". Now jumble it:')
+text = input("Please enter a string of text (the bigger the better): ")
+print('You entered "' + text + '". Now jumble it: ')
 
 #part 1 (in reverse)
 print(text[::-1])
